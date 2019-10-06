1b496a69e1739e4661626ddf837831729bea5a07
so0oshiance <mohamad.farhany@gmail.com>
2019-10-06 21:02:30 +0330

    Add redux to app

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
 import React, {Component} from 'react';
 import {StyleSheet, View} from 'react-native';
+import {connect} from 'react-redux';
+import {
+  addPlace,
+  deletePlace,
+  selectPlace,
+  deselectPlace,
+} from './src/store/actions/index';
 
 import PlaceInput from './src/components/PlaceInput/PlaceInput';
 import PlaceList from './src/components/PlaceList/PlaceList';
 import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
 //static import image
 //import PlaceImage from './src/assets/1.png';
-export default class App extends Component {
-  state = {
+class App extends Component {
+  //Omitted after using redux
+  /* state = {
     places: [],
     selectedPlace: null,
-  };
+  }; */
 
   placeAddedHandler = placeName => {
-    this.setState(prevState => {
+    /*  this.setState(prevState => {
       return {
         places: prevState.places.concat({
           key: Math.random(),
@@ -26,44 +34,51 @@ export default class App extends Component {
           },
         }),
       };
-    });
+    });*/
+    this.props.onAddPlace(placeName);
   };
   placeDeleteHandler = () => {
-    this.setState(prevState => {
+    /* this.setState(prevState => {
       return {
         places: prevState.places.filter(place => {
           return place.key !== prevState.selectedPlace.key;
         }),
         selectedPlace: null,
       };
-    });
+    }); */
+    this.props.onDeletePlace();
   };
   modalClosedHandler = () => {
-    this.setState({
+    /* this.setState({
       selectedPlace: null,
-    });
+    }); */
+    this.props.onDeselectPlace();
   };
   placeSelectedHandler = key => {
-    this.setState(prevState => {
+    /* this.setState(prevState => {
       return {
         selectedPlace: prevState.places.find(place => {
           return place.key === key;
         }),
       };
-    });
+    }); */
+    this.props.onSelectPlace(key);
   };
 
   render() {
     return (
       <View style={styles.container}>
         <PlaceDetail
-          selectedPlace={this.state.selectedPlace}
+          //changed after redux
+          //selectedPlace={this.state.selectedPlace}
+          selectedPlace={this.props.selectedPlace}
           onItemDeleted={this.placeDeleteHandler}
           onModalClosed={this.modalClosedHandler}
         />
         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
         <PlaceList
-          places={this.state.places}
+          //places={this.state.places}
+          places={this.props.places}
           onItemSelected={this.placeSelectedHandler}
         />
       </View>
@@ -80,3 +95,31 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
   },
 });
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    places: state.places.places,
+    selectedPlace: state.places.selectedPlace,
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    onAddPlace: value => {
+      dispatch(addPlace(value));
+    },
+    onDeletePlace: () => {
+      dispatch(deletePlace());
+    },
+    onSelectPlace: key => {
+      dispatch(selectPlace(key));
+    },
+    onDeselectPlace: () => {
+      dispatch(deselectPlace());
+    },
+  };
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(App);
---
+++ src/store/reducers/places.js
@@ -0,0 +1,54 @@
+import {
+  ADD_PLACE,
+  DELETE_PLACE,
+  DESELECT_PLACE,
+  SELECT_PLACE,
+} from '../actions/actionTypes';
+
+const initialState = {
+  places: [],
+  selectedPlace: null,
+};
+// if we do not have any state this will use the initialState
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_PLACE:
+      return {
+        //...state grab all properties of prev state and adds it to new obj
+        //by this, we keep our old state that we need
+        ...state,
+        places: state.places.concat({
+          key: Math.random(),
+          value: action.placeName,
+          image: {
+            uri:
+              'https://bigmemes.funnyjunk.com/pictures/A+beautiful+scene_30057c_7120462.jpg',
+          },
+        }),
+      };
+    case DELETE_PLACE:
+      return {
+        ...state,
+        places: state.places.filter(place => {
+          return place.key !== state.selectedPlace.key;
+        }),
+        selectedPlace: null,
+      };
+    case SELECT_PLACE:
+      return {
+        ...state,
+        selectedPlace: state.places.find(place => {
+          return place.key === action.placeKey;
+        }),
+      };
+    case DESELECT_PLACE:
+      return {
+        ...state,
+        selectedPlace: null,
+      };
+
+    default:
+      return state;
+  }
+};
+export default reducer;
